790bad8aff4d8f54c9318fbc30b29b99a84ee39f
Cael <MR-Addict@qq.com>
2023-11-30 12:31:35 +0800

    feat: automatically reconnect if connection lost

--- code/main/init.h
+++ code/main/init.h
@@ -1,3 +1,8 @@
+void connectionLostCallback(WiFiEvent_t event, WiFiEventInfo_t info) {
+  WiFi.begin(ssid, password);
+  Serial.println("WiFi connection lost. Try to reconnect...");
+}
+
 void init(uint32_t baud_rate, const char* ssid, const char* password) {
   // init led
   pinMode(LED, OUTPUT);
@@ -13,6 +18,7 @@ void init(uint32_t baud_rate, const char* ssid, const char* password) {
   // init wifi
   WiFi.hostname(hostname);
   WiFi.begin(ssid, password);
+  WiFi.onEvent(connectionLostCallback, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
   Serial.printf("Connecting to %s", ssid);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
